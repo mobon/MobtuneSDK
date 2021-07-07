311f2698f53ffcbccc7f050f295d6366a0552ca7
mobon <yikim@enliple.com>
2021-07-07 15:39:25 +0900

    first commit

---
+++ app/src/main/java/sample/sdk/mobtune/GlobalApplication.java
@@ -0,0 +1,19 @@
+package sample.sdk.mobtune;
+
+import android.app.Application;
+import android.os.Build;
+
+import com.mobon.tune.sdk.MobtuneSDK;
+import com.mobon.tune.sdk.MobonLifecycleCallbacks;
+
+public class GlobalApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        MobtuneSDK.init(this,"testSDK");
+        if (Build.VERSION.SDK_INT >= 14) {
+            registerActivityLifecycleCallbacks(new MobonLifecycleCallbacks());
+        }
+    }
+}
